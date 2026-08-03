badb350a6f21753d5b57fab566d61608736e8036
Mete <metesevim02@gmail.com>
2026-08-04 00:19:45 +0300

    feat: add Spring Cloud Gateway module

---
+++ gateway/src/main/java/com/metesevim/staj2026gateway/Staj2026GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.metesevim.staj2026gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Staj2026GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Staj2026GatewayApplication.class, args);
+    }
+
+}
