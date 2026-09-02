c20eadb68761fadb1b9a23fa10881d91a5be32a1
Mete <metesevim02@gmail.com>
2026-09-02 18:15:32 +0300

    feat:liquidbase, haproxy, keepalived added.

--- src/test/java/com/metesevim/staj2026/Staj2026ApplicationTests.java
+++ src/test/java/com/metesevim/staj2026/Staj2026ApplicationTests.java
@@ -3,14 +3,21 @@ package com.metesevim.staj2026;
 import com.metesevim.staj2026.repository.ProductSearchRepository;
 import com.metesevim.staj2026.repository.CouchbaseProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class Staj2026ApplicationTests {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @MockitoBean
     private KafkaTemplate<String, String> kafkaTemplate;
@@ -23,5 +30,15 @@ class Staj2026ApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void liquibaseMigrationsAreApplied() {
+        Integer changeCount = jdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM DATABASECHANGELOG",
+                Integer.class
+        );
+
+        assertEquals(2, changeCount);
     }
 }
